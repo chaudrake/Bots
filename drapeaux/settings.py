910f41eff111a8583d97502b356ec2aaf7383d25
chaudrake <chaudrake123+ascii@hotmail.fr>
2026-05-03 07:55:09 +0200

    Add files via upload

---
+++ drapeaux/settings.py
@@ -0,0 +1,23 @@
+[BotSettings]
+
+# Time in seconds to wait between tweets. Recomended is 1800 and above for 24x7 operation
+time_between_tweets = 10800
+
+# [REPLIT] If running locally or on server set to False
+using_replit = False
+
+# [OTHER SETTINGS] - Change as per your preference
+# Setting include_datetime to True will add the current date+time to the begining of each tweet
+include_datetime = False
+
+# Credentials from developer.twitter.com
+# ONLY SET CREDENTIALS HERE IF RUNNING LOCAL/SERVER
+# FOR REPLIT, PLEASE USE REPLIT SECRETS AS MENTIONED IN STEP 2
+consumer_key = 87anfGLE6YFOrz6ftEerfHca9
+consumer_secret = yqgLILcM03SCCNDEx2G3BHsmJX9GbjW9mlY2tYZsGwAtdAuGI2
+access_token = 1330087066538012675-ma2RFnRmGlW9Hu5gygHg9XPIVO3Y5j
+access_token_secret = Wghf6S7ExJpZECCCJBkXDIGVVhkdrYhuxdJhT9CVRvUFc
+
+## DO NOT EDIT BELOW VALUE
+## THIS IS SET AUTOMATICALLY BY THE BOT
+last_tweet_time = 2026-05-01 08:59:47.525848
